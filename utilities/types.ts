b3985420323388bee390a57ffb209c73bf06bc98
AdiBonofiel95 <adibonofiel@gmail.com>
2024-07-30 03:28:45 +0300

    removed paginationObj

--- utilities/types.ts
+++ utilities/types.ts
@@ -1,23 +1,8 @@
-import { Type } from 'class-transformer';
-import { IsInt, Min } from 'class-validator';
+
 
 export type ResponseObj = {
     statusCode: number,
     timestamp: string,
     path: string,
     response: string | object,
-}
-
-
-
-export class PaginationObj {
-    @Min(1)
-    @Type(() => Number)
-    @IsInt()
-    page?: number;
-
-    @Type(() => Number)
-    @IsInt()
-    @Min(1)
-    limit?: number;
 }
